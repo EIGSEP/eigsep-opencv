244ec31350d7dfa2667edd888fb7c71da76a6e2b
Pranav Karthik <pranavkarthik@berkeley.edu>
2024-07-16 14:55:26 -0700

    bug fix

--- box_position.py
+++ box_position.py
@@ -14,7 +14,7 @@ class BoxPosition:
             3: 4,   # Bottom of the left face borders the top of the top face (currently not used)
             5: 0,   # Bottom of the top face borders the top of the right face (currently not used)
         }
-        self.initial_positions = initial_positions if initial_positions else {}
+        self.initial_positions = initial_positions if initial_positions else []
         self.rotation_count = 0
         self.previous_orientation = None
 
@@ -59,3 +59,51 @@ class BoxPosition:
                 if tag_id in tags.values():
                     detected_faces.add(face)
         return detected_faces
+
+    def calculate_position(self, detections):
+        if not self.initial_positions:
+            return None, None
+
+        positions = []
+        for detection in detections:
+            tag_id = detection.tag_id
+            for face, tags in self.face_tags.items():
+                if tag_id in tags.values():
+                    distance = detection.distance
+                    orientation = detection.orientation
+                    pos = self.get_position_from_tag(face, tag_id, distance, orientation)
+                    positions.append(pos)
+
+        if positions:
+            avg_position = np.mean(positions, axis=0)
+            return avg_position, self.get_orientation_from_tags(detections)
+        else:
+            return None, None
+
+    def get_position_from_tag(self, face, tag_id, distance, orientation):
+        # Simplified example, would need real calculations based on tag and face layout
+        x, y, z = 0, 0, 0
+        if face == 'right':
+            if tag_id == self.face_tags[face]['top']:
+                y = distance
+            elif tag_id == self.face_tags[face]['bottom']:
+                y = -distance
+        elif face == 'bottom':
+            if tag_id == self.face_tags[face]['top']:
+                x = distance
+            elif tag_id == self.face_tags[face]['bottom']:
+                x = -distance
+        elif face == 'left':
+            if tag_id == self.face_tags[face]['top']:
+                y = distance
+            elif tag_id == self.face_tags[face]['bottom']:
+                y = -distance
+        return np.array([x, y, z])
+
+    def get_orientation_from_tags(self, detections):
+        orientations = [d.orientation for d in detections if d.orientation is not None]
+        if orientations:
+            avg_orientation = np.mean(orientations)
+            return avg_orientation
+        else:
+            return None
